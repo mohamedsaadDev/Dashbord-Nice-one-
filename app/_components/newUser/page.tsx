20f3410738bf3c01cc7f33031ffbe57a9a48aac8
Mohamed Saad <131729886+mohamedsaadDev@users.noreply.github.com>
2024-02-22 05:45:13 +0200

    Update page.tsx

--- app/_components/newUser/page.tsx
+++ app/_components/newUser/page.tsx
@@ -6,7 +6,7 @@ function NewUser () {
     const { Users } = useAppContext();
     const newUser = Users.slice(0, 3);
     return (
-        <div className="max-w-sm ml-2 bg-white border border-gray-200 rounded-lg shadow sm:py-5 sm:px-5 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full max-w-xs	 ml-2 bg-white border border-gray-200 rounded-lg shadow sm:py-5 sm:px-5 dark:bg-gray-800 dark:border-gray-700">
             <div className="flex items-center justify-between mb-1">
             <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
                 New User
